0d56b4a55831cc984ce4135e9da9d9cd4b2db34c
TkTnX <timur20-09@mail.ru>
2024-05-06 18:37:42 +0500

    prod2

--- frontend/src/components/PopularPost/PopularPost.tsx
+++ frontend/src/components/PopularPost/PopularPost.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { PostType } from "../Post/Post";
 
-const PopularPost: React.FC<PostType> = ({ imageUrl, title, user, createdAt }) => {
+const PopularPost: React.FC<PostType> = ({
+  imageUrl,
+  title,
+}) => {
   return (
     <li className="max-w-72">
       <img className="rounded" src={imageUrl} alt="car" />
       <h4 className="text-xl font-bold mt-5">{title}</h4>
-      <p className="italic, font-normal text-sm text-slate-500 mt-3">
-        {user.avatarUrl} - {createdAt}
-      </p>
     </li>
   );
 };
